110605303bb7e470c5f8ec913ac6da785766596f
Lorna <lroberts1980@yahoo.com>
2020-03-28 17:58:05 +0000

    refactored code

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function isValidPassword(username, password) {
-    if (username.includes("password")){
-        return false;
-    }else if (username.length <= 7){
-        return false;
-    }else if(username.includes(" ")){
-        return false;
-    }return true;
+    const tooSimilar = username.includes("password");
+    const tooShort = username.length <= 7;
+    const hasSpace = username.includes(" ");
+
+    if (tooSimilar || tooShort || hasSpace) return false;
+    return true;
 };
 
 console.log(isValidPassword('efefefffe','efeegrgegreg'));
